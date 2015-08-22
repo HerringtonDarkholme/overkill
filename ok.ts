3910725322c4ff75a0964fc580c13a4daeeba26a
Herrington Darkholme <nonamesheep1@gmail.com>
2015-08-22 19:54:54 +0800

    initial commit

---
+++ ok.ts
@@ -0,0 +1,170 @@
+/// <reference path='./es6-collections.d.ts' />
+import 'es6-collections'
+
+class Caller<T> {
+  private callers: T[] = []
+  constructor(init?: T) {
+    if (init !== undefined) this.callers.push(init)
+  }
+  withValue(t: T): Function {
+    return (fn: Function) => {
+      this.callers.push(t)
+      let ret = fn()
+      this.callers.pop()
+      return ret
+    }
+  }
+  value() {
+    return this.callers[this.callers.length-1]
+  }
+}
+
+type _ = {}
+type Observer = Obs | Rx<_>
+type Observee = Var<_> | Rx<_>
+
+
+abstract class Signal<T> {
+  protected value: T
+}
+
+// a flag indicates whether to track dependency
+// true when observer calls observee, otherwise false
+var inWatch = false
+export function startObserve(fn: Function) {
+  setImmediate(() => {
+    inWatch = true
+    fn()
+    inWatch = false
+  })
+}
+
+/*
+ * In dependency graph, we have three kinds of node
+ * Var: the root of graph, the source of change. observed by Rx/Obs
+ * Rx: interior node, it observes Var/Rx changes, and notify other Rx/Obs
+ * Obs: leaf node, it should executes side effect,
+ *
+ * In dependency graph, we can define parent node as observee and child node as observer.
+ * Child node will `watch` parent node. Parent node will notify child node when itself changes.
+ *
+ * Var & Rx are observees. Observee has its own value, and a set of observer.
+ * When observees get called in watch phase, it will add the caller to its observer set,
+ * and add itself to caller's observee list.
+ *
+ * Rx & Obs are observers. Observer has a list of observee. Obs is only for side effect.
+ * Observer has a computeValue method in which observer calls observee.
+ * Every time computeValue executes, observer will clean its observee list,
+ * and remove itself from observee's observer set.
+ */
+
+export class Var<T> extends Signal<T> {
+  private observers = new Set<Observer>()
+  constructor(value: T) {
+    super()
+    this.update(value)
+  }
+
+  // observer is agnostic about its expression
+  // so we register dependency when observee get called
+  apply(): T {
+    if (inWatch) {
+      let callSig = caller.value()
+      callSig.watch(this)
+      this.observers.add(callSig)
+    }
+    return this.value
+  }
+
+  // when Var update, all its observers should re-watch
+  update(newValue: T) {
+    if (this.value !== newValue) {
+      inWatch = true
+      this.value = newValue
+      var obs = this.observers
+      this.observers = new Set<any>()
+      obs.forEach(o => o.computeValue())
+      inWatch = false
+    }
+  }
+
+  retireFrom(obs: Observer) {
+    this.observers.delete(obs)
+  }
+}
+
+export class Obs extends Signal<void> {
+  private observees: Array<Observee> = []
+  private expr: () => void
+  constructor(expr: () => void) {
+    super()
+    this.expr = expr
+    startObserve(() => this.computeValue())
+  }
+  computeValue() {
+    for (let sig of this.observees) {
+      sig.retireFrom(this)
+    }
+    this.observees = []
+    caller.withValue(this)(this.expr)
+  }
+  watch(child: Observee) {
+    this.observees.push(child)
+  }
+}
+
+export class Rx<T> extends Signal<T> {
+  private observers = new Set<Observer>()
+  private observees: Array<Observee> = []
+  private expr: () => T
+  constructor(expr: () => T) {
+    super()
+    this.expr = expr
+    startObserve(() => this.computeValue())
+  }
+  computeValue() {
+    for (let sig of this.observees) {
+      sig.retireFrom(this)
+    }
+    this.observees = []
+    let newValue = caller.withValue(this)(this.expr)
+    if (this.value !== newValue) {
+      this.value = newValue
+      let obs = this.observers
+      this.observers = new Set<any>()
+      obs.forEach(o => o.computeValue())
+    }
+  }
+  apply(): T {
+    if (inWatch) {
+      let callSig = caller.value()
+      this.observers.add(callSig)
+      callSig.watch(this)
+    }
+    return this.value
+  }
+  watch(child: Observee) {
+    // only interior node will form cycle
+    if (child instanceof Rx) {
+      this.assertNoCyclic(child)
+    }
+    this.observees.push(child)
+  }
+
+  retireFrom(obs: Observer) {
+    this.observers.delete(obs)
+  }
+
+  assertNoCyclic(child: Rx<_>) {
+    if (this.observers.has(child)) {
+      throw new Error('cyclic error')
+    }
+    this.observers.forEach(p => {
+      if (p instanceof Rx) {
+        p.assertNoCyclic(child)
+      }
+    })
+  }
+}
+
+export var caller = new Caller<Observer>(new Obs(function(){}))
---
+++ test.js
@@ -0,0 +1,130 @@
+/* globals describe, it */
+'use strict'
+var assert = require('assert')
+var ok = require('./ok')
+var Rx = ok.Rx
+var Obs = ok.Obs
+var Var = ok.Var
+var caller = ok.caller
+var startObserve = ok.startObserve
+
+describe('Var', function() {
+  var CONST1 = 42
+  var CONST2 = 24
+
+  it('should return inital value', function() {
+    var a = new Var(CONST1)
+    assert(a.apply() === CONST1)
+  })
+
+  it('should update value', function () {
+    var a = new Var(CONST1)
+    a.update(CONST2)
+    assert(a.apply() === CONST2)
+  })
+
+  var mockObserver = {
+    observee: null,
+    getCalledTimes: 0,
+    watch: function(v) { this.observee = v },
+    computeValue: function() {
+      this.getCalledTimes++
+    },
+    mockLink: function(obs, cb) {
+      startObserve(function() {
+        caller.withValue(mockObserver)(function() {
+          obs.apply()
+        })
+        cb()
+      })
+    }
+  }
+
+  it('should link observer', function(done) {
+    var a = new Var(CONST1)
+    mockObserver.getCalledTimes = 0
+    mockObserver.observee = null
+    mockObserver.mockLink(a, function() {
+      assert(mockObserver.observee === a)
+      done()
+    })
+  })
+  it('should call observer when changes', function(done) {
+    var a = new Var(CONST1)
+    mockObserver.getCalledTimes = 0
+    mockObserver.observee = null
+    mockObserver.mockLink(a, function() {
+      a.update(CONST2)
+      assert(a.apply() === CONST2)
+      assert(mockObserver.getCalledTimes === 1)
+      done()
+    })
+  })
+
+  it('should not call observer when no changes', function(done) {
+    var a = new Var(CONST1)
+    mockObserver.getCalledTimes = 0
+    mockObserver.observee = null
+    mockObserver.mockLink(a, function() {
+      a.update(CONST1)
+      assert(a.apply() === CONST1)
+      assert(mockObserver.getCalledTimes === 0)
+      done()
+    })
+  })
+})
+
+describe('Obs', function() {
+  it('should make side effect', function(done) {
+    var sideEffectDone = false
+    var obs = new Obs(function() {
+      sideEffectDone = true
+    })
+    setTimeout(function() {
+      assert(sideEffectDone)
+      obs = null
+      done()
+    }, 1)
+  })
+
+  it('should has observee', function(done) {
+    var sig = new Var(123)
+    var obs = new Obs(function() {
+      sig.apply()
+    })
+    setTimeout(function() {
+      assert(obs.observees.length > 0)
+      done()
+    }, 1)
+  })
+
+  it('should remove observee', function(done) {
+    var sig = new Var(123)
+    var isTracking = false;
+    var obs = new Obs(function() {
+      if (isTracking) {
+        sig.apply()
+      }
+    })
+
+    setTimeout(function() {
+      assert(obs.observees.length === 0)
+      isTracking = true
+      sig.update(456)
+      setTimeout(function() {
+        assert(obs.observees.length === 1)
+        done()
+      }, 2)
+    }, 1)
+
+  })
+
+})
+
+// var a = new Rx(() => b.apply())
+// var b = new Rx(() => c.apply())
+// var c = new Rx(() => a.apply())
+
+// setImmediate(() => {
+//   console.log(c.apply())
+// })
